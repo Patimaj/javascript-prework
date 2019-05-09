7d58c06ecd035cdc1529cffe19bc20dace0fa031
Patrycja Majewska <slabynysa@o2.pl>
2019-05-09 20:09:31 +0200

    add script

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var computerMove, playerMove;
+playerMove = 'papier';
+computerMove = 'kamień';
+printMessage((['Zagrałem ',computerMove,'! Jeśli Twój ruch to papier, to wygrywasz! ','zagrałem ',playerMove,'! Jeśli Twój ruch to nożyczki, to wygrywasz!'].join('')));
